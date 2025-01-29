6085eb537fff44bf01b1be77fadfe8b09c621120
manuel <manuelpozor2004@gmail.com>
2025-01-29 17:02:36 +0100

    Nuevos Libros

--- BIBLIOTECA/src/Biblioteca.java
+++ BIBLIOTECA/src/Biblioteca.java
@@ -6,9 +6,13 @@ public class Biblioteca {
     static List<Usuario> usuarios = new ArrayList<>();
 
     public static void inicializarDatos() {
-        libros.add(new Libro("Cien años de soledad", "Gabriel García Márquez", "Ficción"));
+        libros.add(new Libro("IT", "Stephen King", "Terror"));
         libros.add(new Libro("El principito", "Antoine de Saint-Exupéry", "Fábula"));
-        libros.add(new Libro("1984", "George Orwell", "Ciencia ficción"));
+        libros.add(new Libro("Cumbre Borrascosas", "Emily Bronte", "Drama"));
+        libros.add(new Libro("Drácula", "Bram Stoker", "Terror"));
+        libros.add(new Libro("Alicia en el País de las Maravillas", "Lewis Carrol", "Infantil"));
+        libros.add(new Libro("Alicia en el País de las Maravillas", "Lewis Carrol", "Infantil"));
+        libros.add(new Libro("Romeo y Julieta", "William Shakespeare", "Drama"));
 
         usuarios.add(new Usuario("admin", "admin123", true));
         usuarios.add(new Usuario("usuario1", "pass123", false));
@@ -17,18 +21,6 @@ public class Biblioteca {
     public static void agregarLibro(String titulo, String autor, String categoria) {
         libros.add(new Libro(titulo, autor, categoria));
         System.out.println("Libro agregado exitosamente.");
-    }
-
-    public static void eliminarLibro(String titulo) {
-        for (Iterator<Libro> it = libros.iterator(); it.hasNext(); ) {
-            Libro libro = it.next();
-            if (libro.titulo.equalsIgnoreCase(titulo)) {
-                it.remove();
-                System.out.println("Libro eliminado exitosamente.");
-                return;
-            }
-        }
-        System.out.println("No se encontró el libro.");
     }
 
     public static void mostrarLibros() {
@@ -45,5 +37,17 @@ public class Biblioteca {
                 System.out.println(libro);
             }
         }
+    }
+
+    public static void eliminarLibro(String titulo) {
+        for (Iterator<Libro> it = libros.iterator(); it.hasNext(); ) {
+            Libro libro = it.next();
+            if (libro.titulo.equalsIgnoreCase(titulo)) {
+                it.remove();
+                System.out.println("Libro eliminado exitosamente.");
+                return;
+            }
+        }
+        System.out.println("No se encontró el libro.");
     }
 }
